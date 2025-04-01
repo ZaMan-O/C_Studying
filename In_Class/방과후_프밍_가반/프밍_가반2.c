06bddb6e908b99fc0a1a8eec2cd8cb855a1bf1f0
ZaMan <roblery128@gmail.com>
2025-04-01 18:11:26 +0900

    방과후에 한것

---
+++ In_Class/방과후_프밍_가반/프밍_가반2.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int numbers[8] = { 13,15,2,19,60,46,248,153 };
+	int lowest;
+	int lowest_index;
+
+	for (int i = 0;i < 8;i++) {
+		lowest = INT_MAX;
+		lowest_index = 0;
+		for (int ii = i;ii < 8;ii++) {
+			if (lowest > numbers[ii]) lowest = numbers[ii], lowest_index = ii;
+		}
+		numbers[lowest_index] = numbers[i];
+		numbers[i] = lowest;
+	}
+	
+	for (int i = 0;i < 8;i++) printf("%d\n", numbers[i]);
+}
